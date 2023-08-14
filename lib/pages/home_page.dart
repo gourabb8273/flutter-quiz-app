0adbc5012f04749924a78625b44960b2e7f5abfc
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-08-14 13:12:40 +0530

    adding quiz and result count

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../store/user.dart';
+import '../store/quiz.dart';
 import '../widgets/quiz_tab.dart';
 import '../widgets/result_tab.dart';
 
 class TabNavigationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final quizResponse =
+        Provider.of<UserStore>(context, listen: false).userQuizResponse;
+    final quizTopic =
+        Provider.of<QuizStore>(context, listen: false).quizTopics;
     return DefaultTabController(
-      length: 2, 
+      length: 2,
       child: Scaffold(
         body: Column(
           children: [
             Container(
-              color: Colors.orange[400], 
+              color: Colors.orange[400],
               child: TabBar(
-                indicatorColor: Color.fromARGB(255, 26, 25, 25), 
-                labelColor: const Color.fromARGB(255, 0, 0, 0), 
+                indicatorColor: Color.fromARGB(255, 26, 25, 25),
+                labelColor: const Color.fromARGB(255, 0, 0, 0),
                 unselectedLabelColor: Color.fromARGB(255, 39, 35, 35),
                 tabs: [
                   Tab(
-                    text: 'Quiz',
+                    text: 'Quiz (${quizTopic.length})',
                   ),
                   Tab(
-                    text: 'Result',
+                    text: 'Result (${quizResponse.length})',
                   ),
                 ],
               ),
--- lib/widgets/result_tab.dart
+++ lib/widgets/result_tab.dart
@@ -8,12 +8,14 @@ class ResultTab extends StatelessWidget {
   Widget build(BuildContext context) {
     final quizResponse =
         Provider.of<UserStore>(context, listen: false).userQuizResponse;
+
     // Sort quizResponse list by date
     quizResponse.sort((a, b) {
       DateTime dateA = DateTime.parse(a['date']);
       DateTime dateB = DateTime.parse(b['date']);
       return dateB.compareTo(dateA);
     });
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(0),
@@ -23,63 +25,85 @@ class ResultTab extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              for (var result in quizResponse)
+              SizedBox(height: 10), // Add top margin
+              if (quizResponse.isEmpty)
                 Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(vertical: 180),
                   padding: EdgeInsets.all(16),
-                  margin: EdgeInsets.only(bottom: 16),
-                  decoration: BoxDecoration(
-                    color: Colors.orange.shade100,
+                  decoration: BoxDecoration(                    
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: CircularProgressIndicator(
-                          value: double.parse(result['percentage']) / 100,
-                          strokeWidth: 5,
-                          backgroundColor: Colors.grey.shade300,
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.orange),
-                        ),
-                      ),
-                      SizedBox(width: 16),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${result['topic']}',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Percentage: ${result['percentage']}%',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          Text(
-                            'Attempted on ${result['date']}',
-                            style: TextStyle(fontSize: 14),
-                          ),
-                        ],
-                      ),
+                  child: Text(
+                    'No results available. Please start your quiz now.',
+                    style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 50, 49, 49)),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              else
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    for (var result in quizResponse)
                       Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                        padding: EdgeInsets.all(16),
+                        margin: EdgeInsets.only(bottom: 16),
                         decoration: BoxDecoration(
-                          color: result['isPass'] ? Colors.green : Colors.red,
+                          color: Colors.orange.shade100,
                           borderRadius: BorderRadius.circular(8),
                         ),
-                        child: Text(
-                          result['isPass'] ? 'Pass' : 'Fail',
-                          style: TextStyle(color: Colors.white),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: CircularProgressIndicator(
+                                value: double.parse(result['percentage']) / 100,
+                                strokeWidth: 5,
+                                backgroundColor: Colors.grey.shade300,
+                                valueColor:
+                                    AlwaysStoppedAnimation<Color>(Colors.orange),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${result['topic']}',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Percentage: ${result['percentage']}%',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                Text(
+                                  'Attempted on ${result['date']}',
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              padding:
+                                  EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: result['isPass'] ? Colors.green : Colors.red,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                result['isPass'] ? 'Pass' : 'Fail',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                  ],
                 ),
             ],
           ),
